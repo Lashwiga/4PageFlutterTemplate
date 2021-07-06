6f1e124cd60d5d7873c50571015d6097d567549c
Wade Cheng <2014wcheng@gmail.com>
2021-07-05 17:00:11 -0700

    add comments

--- lib/NotificationScreenListTile.dart
+++ lib/NotificationScreenListTile.dart
@@ -1,29 +1,26 @@
+// NOTE: This file makes a custom Widget based off of the ExpansionTile widget
+
 import 'package:flutter/material.dart';
 
 class NotificationScreenListTile extends StatelessWidget {
+  // require title and description parameters
   NotificationScreenListTile({required this.title, required this.description});
 
   final String title;
   final String description;
 
+  // build an custom ExpansionTile
   @override
   Widget build(BuildContext context) {
   return ExpansionTile(
-    // key: PageStorageKey(this.widget.headerTitle),
-
     title: Text(title,style: TextStyle(fontSize: 18),),
-
+    // children shows up when expanded
     children: [
       Align(
         alignment: Alignment.topLeft,
-        child: Text(description, style: TextStyle(fontSize: 14),)),
+        child: Text(description, style: TextStyle(fontSize: 14),)
+      ),
     ],
-
-    // onExpansionChanged: (value){
-    //   setState(() {
-    //     isExpand=value;
-    //   });
-    // },
     );
   }
 }
